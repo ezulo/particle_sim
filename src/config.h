311919e6d6c545331106786e7dfb187fabb678b5
ezulo <ezuloaga0@gmail.com>
2025-10-27 13:26:23 -0400

    Config tweaks

--- src/config.h
+++ src/config.h
@@ -8,17 +8,17 @@
 #define PARTICLE_DISABLE_STOP       1
 
 /* Resolution Options */
-#define FRAMERATE       30.0f 
+#define FRAMERATE       60.0f 
 #define WINDOW_SIZE_X   1000
 #define WINDOW_SIZE_Y   1000
 
-/* Initial velocity max/min */
-#define V0_MAX 5.0f
+/* Initial speed max/min */
+#define V0_MAX 7.25f
 
 /* Sim options */
 #define PARTICLE_RADIUS                     5.0f
 #define PARTICLE_COLOR                      sf::Color::Red
-#define PARTICLE_QUANTITY                   2000
+#define PARTICLE_QUANTITY                   300
 #define PARTICLE_TRACER                     7
 #define PARTICLE_FIELD_CENTER_X             500.0f
 #define PARTICLE_FIELD_CENTER_Y             500.0f
